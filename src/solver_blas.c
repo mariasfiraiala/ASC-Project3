7120362b476c36575e6f452dc7e426903faaf73c
Maria Sfiraiala <maria.sfiraiala@gmail.com>
2024-05-20 11:14:09 +0300

    src: Add blas algorithm
    Signed-off-by: Maria Sfiraiala <maria.sfiraiala@gmail.com>

--- src/solver_blas.c
+++ src/solver_blas.c
@@ -3,11 +3,29 @@
  * 2024 Spring
  */
 #include "utils.h"
+#include <cblas.h>
 
-/* 
- * Add your BLAS implementation here
- */
 double* my_solver(int N, double *A, double *B) {
-	printf("BLAS SOLVER\n");
-	return NULL;
+	int n = N * N;
+
+	double *a_tb = calloc(n, sizeof(*a_tb));
+	cblas_dcopy(n, B, 1, a_tb, 1);
+	cblas_dtrmm(CblasRowMajor, CblasLeft, CblasUpper, CblasTrans, CblasNonUnit, N, N, 1, A, N, a_tb, N);
+
+	double *ba = calloc(n, sizeof(*ba));
+	cblas_dcopy(n, B, 1, ba, 1);
+	cblas_dtrmm(CblasRowMajor, CblasRight, CblasUpper, CblasNoTrans, CblasNonUnit, N, N, 1, A, N, ba, N);
+
+	double *sum = calloc(n, sizeof(*sum));
+	cblas_dcopy(n, ba, 1, sum, 1);
+	cblas_daxpy(n, 1, a_tb, 1, sum, 1);
+
+	double *res = calloc(n, sizeof(*res));
+	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, N, N, N, 1, sum, N, B, N, 0, res, N);
+
+	free(a_tb);
+	free(ba);
+	free(sum);
+
+	return res;
 }
